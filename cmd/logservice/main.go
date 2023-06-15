271ebf53f8d08281989cd70b9f32d9fd692a3aa7
kokodayou2000 <1611601667@qq.com>
2023-06-15 22:59:20 +0800

    修改服务名赋值方式

--- cmd/logservice/main.go
+++ cmd/logservice/main.go
@@ -12,9 +12,9 @@ import (
 func main() {
 	log.Run("./distributed.log")
 	host, port := "localhost", "4000"
-	serviceAddress := fmt.Sprintf("http://%s:%s", host, port)
+	serviceAddress := fmt.Sprintf("http://%v:%v", host, port)
 	r := registry.Registration{
-		ServiceName: "Log Service",
+		ServiceName: registry.LogService,
 		ServiceURL:  serviceAddress,
 	}
 	ctx, err := service.Start(
